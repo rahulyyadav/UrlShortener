f63b11900ce3545844ca16facab8aaa3a9f18c46
Rahul Yadav <rahulyyadav21@gmail.com>
2025-02-22 17:50:48 +0530

    updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Ensures the project builds as a static site
+  distDir: ".vercel/output", // Defines the output directory for Cloudflare Pages
+  images: {
+    unoptimized: true, // Cloudflare Pages does not support Next.js image optimization
+  },
 };
 
 export default nextConfig;
